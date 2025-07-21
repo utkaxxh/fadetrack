4fb9ca5464f17880d1a941526c77cd9eb5209065
Utkarsh Agrawal <utkarsh_agrawal@rocketmail.com>
2025-07-20 19:57:26 -0700

    update: redirect after signing in

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,25 @@
 "use client";
+
 import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
 import { supabase } from "../../components/supabaseClient";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function LoginPage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const { data: listener } = supabase.auth.onAuthStateChange((event, session) => {
+      if (session && session.user) {
+        router.replace("/");
+      }
+    });
+    return () => {
+      listener?.subscription.unsubscribe();
+    };
+  }, [router]);
+
   return (
     <div className="flex justify-center items-center min-h-screen bg-gradient-to-br from-black via-gray-900 to-gray-800">
       <div className="bg-gray-900 p-8 rounded-xl shadow-lg w-full max-w-md">
